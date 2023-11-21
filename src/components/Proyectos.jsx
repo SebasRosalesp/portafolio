6f0e4c25923c8989f42dd7aa8e1f2d66606f0751
SebasRosalesp <srosalesparra@gmail.com>
2023-11-20 18:59:02 -0600

    Primera etapa

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import './Proyectos.css'
 
 const Proyectos = ()=>{
 
     return(
         <>
-            <div>
-                <p>Proyectos</p>
+            <div className="proyectContainer">
+                <div className="proyect">
+                    <a href="https://swapi-react.onrender.com/"><img alt="Proyecto Swpai" src="/src/assets/swapi.png"/> Swapi React</a>
+                    <div className="delay">
+                        <p>Proyecto desarrollado en React enfocado en el uso de un API externo para procesar sus datos y mostrarlos de la manera adecuada, en este caso mediante un carrusel</p>
+                    </div>
+                </div>
             </div>
         </>
     )
