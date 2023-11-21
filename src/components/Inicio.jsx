66ef32726c9f694e23081a74fd9b429f36e05033
SebasRosalesp <srosalesparra@gmail.com>
2023-11-20 22:03:42 -0600

    2.3

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -11,25 +11,25 @@ const Inicio = () =>{
                     <p>Bienvenid@ a mi portafolio, espero que disfrutes el contenido.</p>
                 </div>
                 <div className="cvContainer">
-                <a className="bajarA" href="/files/Professional CV Resume.pdf"> <button className="bajarB"><img className="bajarI" src="/public/assets/Bajar.png" alt="bajar"/> Descarga mi CV</button></a>
+                <a className="bajarA" href="/files/Professional CV Resume.pdf"> <button className="bajarB"><img className="bajarI" src="/assets/Bajar.png" alt="bajar"/> Descarga mi CV</button></a>
                 </div>
                </div>
                <div className="tecno"><p className="tecnoText">Tecnologías que domino</p></div>
                 <div className="technologies">
                     <div className="html tImg">
-                        <div><img alt="html 5" src="/public/assets/html-5.png"/>HTML5</div>
+                        <div><img alt="html 5" src="/assets/html-5.png"/>HTML5</div>
                     </div>
                     <div className="css tImg">
-                        <div><img alt="css 3" src="/public/assets/css-3.png"/>CSS3</div>
+                        <div><img alt="css 3" src="/assets/css-3.png"/>CSS3</div>
                     </div>
                     <div className="js tImg">
-                        <div><img alt="JavaScript" src="/public/assets/js.png"/>JavaScript</div>
+                        <div><img alt="JavaScript" src="/assets/js.png"/>JavaScript</div>
                     </div>
                     <div className="react tImg">
-                        <div><img alt="react" src="/public/assets/React.png"/>React</div>
+                        <div><img alt="react" src="/assets/React.png"/>React</div>
                     </div>
                     <div className="vite tImg">
-                        <div><img alt="vite" src="/public/assets/vite-js-logo.svg"/>Vite</div>
+                        <div><img alt="vite" src="/assets/vite-js-logo.svg"/>Vite</div>
                     </div>
                     
                 </div>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,8 +3,8 @@ import './Logo.css'
 const Logo = ()=>{
     return(
         <div className="header">
-            <img alt="Logo" className="logo" src="/public/assets/Logo.png"/>
-        <img alt="Foto" className="foto" src="/public/assets/Foto.jpg"/>
+            <img alt="Logo" className="logo" src="/assets/Logo.png"/>
+        <img alt="Foto" className="foto" src="/assets/Foto.jpg"/>
         </div>
     )
 }
